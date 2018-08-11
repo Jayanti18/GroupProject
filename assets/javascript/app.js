568ed2c0a10ace1761f6df5933562cab8918cd6f
NikzadE <arshad.nikzad@gmail.com>
2018-08-11 13:05:05 -0400

    Adding basic HTML and CSS

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,16 @@
 console.log("hello");
+
+//Musictopia!
+//Layout
+//Top Ten Random Songs
+//Search Button with Input
+//Individual songs search
+document.ready(function (){
+    $("#advancedsSearch").on("click", function (){
+        event.preventDefault();
+        alert("OK");
+        
+        $("#advancedDiv").show();
+        
+    });
+});
