977dcc9246aa29f28419cb95c3034de807756ed6
Jay <jevonte_n@outlook.com>
2018-08-19 16:57:15 -0400

    This build here just display lyrics in the space where the song choice were. Its not how I want it but just want to let you all know I basically got the API to do all we need it too

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,7 +12,8 @@ $("#submit").click(function getLyrics() {
         data: {
             apikey: "8d9b55038036aa828dc45b390ee08d45",
             q_artist: artistSearch,
-            s_track_rating: "ASC",
+            // s_track_rating: "ASC",
+            f_has_lyrics: 1,
             format: "jsonp",
             callback: "jsonp_callback"
         },
@@ -29,18 +30,66 @@ $("#submit").click(function getLyrics() {
             // console.log("this is the length", musicResults.track_list[i].length);
             for (var i=0; i< musicResults.track_list.length; i++) {
                 console.log("this is the length", musicResults.track_list[i]);
-
-                $("#results").append( "<br>" + musicResults.track_list[i].track.track_name);
+                
+                var trackId= musicResults.track_list[i].track.track_id
+                var song = $("<p>").text(musicResults.track_list[i].track.track_name);
+                song.attr("id", trackId);
+                $("#results").append( song);
+                console.log(song);
                 console.log( "<br>" + musicResults.track_list[i].track.track_id);
+                var trackId= musicResults.track_list[i].track.track_id
 
-                    
+            
+                
+                console.log(trackId);
 
                     
             }
+            
+
+                $.ajax({
+                    type: "GET",
+                    data: {
+                        apikey:"8d9b55038036aa828dc45b390ee08d45",
+                        track_id: trackId,
+                        format:"jsonp",
+                        callback:"jsonp_callback"
+                    },
+                    url: "https://api.musixmatch.com/ws/1.1/track.lyrics.get",
+                    dataType: "jsonp",
+                    jsonpCallback: 'jsonp_callback',
+                    contentType: 'application/json',
+                    success: function(data) {
+                       console.log(data); 
+                       console.log(data.message.body.lyrics.lyrics_body);
+                       
+                       $("#results").text(data.message.body.lyrics.lyrics_body);
+
+                
+                    }
+                    });
 
 
         }
     });
-
-
-});
+        // $.ajax({
+        //     type: "GET",
+        //     data: {
+        //         apikey:"8d9b55038036aa828dc45b390ee08d45",
+        //         track_id: trackId,
+        //         format:"jsonp",
+        //         callback:"jsonp_callback"
+        //     },
+        //     url: "https://api.musixmatch.com/ws/1.1/track.lyrics.get",
+        //     dataType: "jsonp",
+        //     jsonpCallback: 'jsonp_callback',
+        //     contentType: 'application/json',
+        //     success: function(data) {
+        //        console.log(data); 
+        
+        //     }
+        //     });
+        
+    });
+    
+     
